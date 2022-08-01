926e2a67f241fc361e4f3893ead20d49ba2268ee
HyperSys <66301237+HyperSkys@users.noreply.github.com>
2022-08-01 11:55:16 -0600

    place

--- src/main/java/dev/hyperskys/leetcode/Leetcode.java
+++ src/main/java/dev/hyperskys/leetcode/Leetcode.java
@@ -2,6 +2,6 @@ package dev.hyperskys.leetcode;
 
 public class Leetcode {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Placeholder");
     }
 }
